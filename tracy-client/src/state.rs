43af427a73380d4f87bf067b9574f9dc3db087f6
Simonas Kazlauskas <git@kazlauskas.me>
2024-01-28 01:13:32 +0200

    manual-init: make it compile, add an additional ci configuration

--- tracy-client/src/state.rs
+++ tracy-client/src/state.rs
@@ -1,58 +1,4 @@
 use crate::Client;
-#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-use std::sync::atomic::Ordering;
-
-/// Enabling `Tracy` when it is already enabled, or Disabling when it is already disabled will
-/// cause applications to crash. I personally think it would be better if this was a sort-of
-/// reference counted kind-of thing so you could enable as many times as you wish and disable
-/// just as many times without any reprecursions. At the very least this could significantly
-/// help tests.
-///
-/// We can also try to implement something like this ourselves. To do this we'd want to track 4
-/// states that construct a following finite state machine:
-///
-/// ```text
-///     0 = disabled  -> 1 = enabling
-///         ^                v
-///     3 = disabling <- 2 = enabled
-/// ```
-///
-/// And also include a reference count somewhere in there. Something we can throw in a static
-/// would be ideal.
-///
-/// Alas, Tracy's extensive use of thread-local storage presents us with another problem – we must
-/// start up and shut down the client within the same thread. A most straightforward soution for
-/// that would be to run a separate thread that would be dedicated entirely to just starting up and
-/// shutting down the profiler.
-///
-/// All that seems like a major pain to implement, and so we’ll punt on disabling entirely until
-/// somebody comes with a good use-case warranting that sort of complexity.
-#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-#[cfg(not(loom))]
-static CLIENT_STATE: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
-#[cfg(loom)]
-loom::lazy_static! {
-    static ref CLIENT_STATE: loom::sync::atomic::AtomicUsize =
-        loom::sync::atomic::AtomicUsize::new(0);
-}
-
-#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-const STATE_STEP: usize = 1; // Move forward by 1 step in the FSM
-#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-const STATE_DISABLED: usize = 0;
-#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-const STATE_ENABLING: usize = STATE_DISABLED + STATE_STEP;
-#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-const STATE_ENABLED: usize = STATE_ENABLING + STATE_STEP;
-
-#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-#[inline(always)]
-fn spin_loop() {
-    #[cfg(loom)]
-    loom::thread::yield_now();
-    #[cfg(not(loom))]
-    std::hint::spin_loop();
-}
 
 /// Client initialization and lifetime management.
 impl Client {
@@ -79,53 +25,12 @@ impl Client {
     /// // }
     /// ```
     pub fn start() -> Self {
+        #[cfg(not(feature = "enable"))]
+        return Self(());
         #[cfg(all(feature = "enable", feature = "manual-lifetime"))]
-        {
-            let mut old_state = CLIENT_STATE.load(Ordering::Relaxed);
-            loop {
-                match old_state {
-                    STATE_ENABLED => return Self(()),
-                    STATE_ENABLING => {
-                        while !Self::is_running() {
-                            spin_loop();
-                        }
-                        return Self(());
-                    }
-                    STATE_DISABLED => {
-                        let result = CLIENT_STATE.compare_exchange_weak(
-                            old_state,
-                            STATE_ENABLING,
-                            Ordering::Relaxed,
-                            Ordering::Relaxed,
-                        );
-                        if let Err(next_old_state) = result {
-                            old_state = next_old_state;
-                            continue;
-                        } else {
-                            unsafe {
-                                // SAFE: This function must not be called if the profiler has
-                                // already been enabled. While in practice calling this function
-                                // multiple times will only serve to trigger an assertion, we
-                                // cannot exactly rely on this, since it is an undocumented
-                                // behaviour and the upstream might very well just decide to invoke
-                                // UB instead. In the case there are multiple copies of
-                                // `tracy-client` this invariant is not actually maintained, but
-                                // otherwise this is sound due to the `ENABLE_STATE` that we
-                                // manage.
-                                //
-                                // TODO: we _could_ define `ENABLE_STATE` in the `-sys` crate...
-                                let () = sys::___tracy_startup_profiler();
-                                CLIENT_STATE.store(STATE_ENABLED, Ordering::Release);
-                                return Self(());
-                            }
-                        }
-                    }
-                    _ => unreachable!(),
-                }
-            }
-        }
-        #[cfg(not(all(feature = "enable", feature = "manual-lifetime")))]
-        Client(())
+        return manual_lifetime::start();
+        #[cfg(all(feature = "enable", not(feature = "manual-lifetime")))]
+        return Self(());
     }
 
     /// Obtain a client handle, but only if the client is already running.
@@ -140,15 +45,14 @@ impl Client {
 
     /// Is the client already running?
     pub fn is_running() -> bool {
-        #![allow(unreachable_code)]
         #[cfg(not(feature = "enable"))]
         return true; // If the client is disabled, produce a "no-op" one so that users don’t need
                      // to wory about conditional use of the instrumentation in their own code.
-        #[cfg(not(feature = "manual-lifetime"))]
+        #[cfg(all(feature = "enable", feature = "manual-lifetime"))]
+        return manual_lifetime::is_running();
+        #[cfg(all(feature = "enable", not(feature = "manual-lifetime")))]
         return true; // The client is started in life-before-main (or upon first use in case of
                      // `delayed-init`
-        #[cfg(feature = "manual-lifetime")]
-        return CLIENT_STATE.load(Ordering::Relaxed) == STATE_ENABLED;
     }
 }
 
@@ -161,14 +65,113 @@ impl Clone for Client {
     }
 }
 
-#[cfg(all(test, feature = "enable"))]
-mod test {
-    use super::*;
+#[cfg(all(feature = "enable", feature = "manual-lifetime"))]
+mod manual_lifetime {
+    use std::sync::atomic::Ordering;
+    /// Enabling `Tracy` when it is already enabled, or Disabling when it is already disabled will
+    /// cause applications to crash. I personally think it would be better if this was a sort-of
+    /// reference counted kind-of thing so you could enable as many times as you wish and disable
+    /// just as many times without any reprecursions. At the very least this could significantly
+    /// help tests.
+    ///
+    /// We can also try to implement something like this ourselves. To do this we'd want to track 4
+    /// states that construct a following finite state machine:
+    ///
+    /// ```text
+    ///     0 = disabled  -> 1 = enabling
+    ///         ^                v
+    ///     3 = disabling <- 2 = enabled
+    /// ```
+    ///
+    /// And also include a reference count somewhere in there. Something we can throw in a static
+    /// would be ideal.
+    ///
+    /// Alas, Tracy's extensive use of thread-local storage presents us with another problem – we must
+    /// start up and shut down the client within the same thread. A most straightforward soution for
+    /// that would be to run a separate thread that would be dedicated entirely to just starting up and
+    /// shutting down the profiler.
+    ///
+    /// All that seems like a major pain to implement, and so we’ll punt on disabling entirely until
+    /// somebody comes with a good use-case warranting that sort of complexity.
+    #[cfg(not(loom))]
+    static CLIENT_STATE: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
+    #[cfg(loom)]
+    loom::lazy_static! {
+        static ref CLIENT_STATE: loom::sync::atomic::AtomicUsize =
+            loom::sync::atomic::AtomicUsize::new(0);
+    }
 
-    #[test]
-    fn state_transitions() {
-        assert_eq!(0, STATE_DISABLED);
-        assert_eq!(STATE_DISABLED.wrapping_add(STATE_STEP), STATE_ENABLING);
-        assert_eq!(STATE_ENABLING.wrapping_add(STATE_STEP), STATE_ENABLED);
+    const STATE_STEP: usize = 1; // Move forward by 1 step in the FSM
+    const STATE_DISABLED: usize = 0;
+    const STATE_ENABLING: usize = STATE_DISABLED + STATE_STEP;
+    const STATE_ENABLED: usize = STATE_ENABLING + STATE_STEP;
+
+    #[inline(always)]
+    fn spin_loop() {
+        #[cfg(loom)]
+        loom::thread::yield_now();
+        #[cfg(not(loom))]
+        std::hint::spin_loop();
+    }
+
+    pub(super) fn start() -> super::Client {
+        let mut old_state = CLIENT_STATE.load(Ordering::Relaxed);
+        loop {
+            match old_state {
+                STATE_ENABLED => return super::Client(()),
+                STATE_ENABLING => {
+                    while !is_running() {
+                        spin_loop();
+                    }
+                    return super::Client(());
+                }
+                STATE_DISABLED => {
+                    let result = CLIENT_STATE.compare_exchange_weak(
+                        old_state,
+                        STATE_ENABLING,
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    );
+                    if let Err(next_old_state) = result {
+                        old_state = next_old_state;
+                        continue;
+                    } else {
+                        unsafe {
+                            // SAFE: This function must not be called if the profiler has
+                            // already been enabled. While in practice calling this function
+                            // multiple times will only serve to trigger an assertion, we
+                            // cannot exactly rely on this, since it is an undocumented
+                            // behaviour and the upstream might very well just decide to invoke
+                            // UB instead. In the case there are multiple copies of
+                            // `tracy-client` this invariant is not actually maintained, but
+                            // otherwise this is sound due to the `ENABLE_STATE` that we
+                            // manage.
+                            //
+                            // TODO: we _could_ define `ENABLE_STATE` in the `-sys` crate...
+                            let () = sys::___tracy_startup_profiler();
+                            CLIENT_STATE.store(STATE_ENABLED, Ordering::Release);
+                            return super::Client(());
+                        }
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    pub(super) fn is_running() -> bool {
+        return CLIENT_STATE.load(Ordering::Relaxed) == STATE_ENABLED;
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn state_transitions() {
+            assert_eq!(0, STATE_DISABLED);
+            assert_eq!(STATE_DISABLED.wrapping_add(STATE_STEP), STATE_ENABLING);
+            assert_eq!(STATE_ENABLING.wrapping_add(STATE_STEP), STATE_ENABLED);
+        }
     }
 }
